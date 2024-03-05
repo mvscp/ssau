9a1015d5bf39a0d6424692ae629937423b6eb4ba
Roman <roma2255@bk.ru>
2024-03-05 17:13:52 +0400

    update

--- Проектирование БД/LR1/data.sql
+++ Проектирование БД/LR1/data.sql
@@ -6,14 +6,6 @@ INSERT INTO mountain (name, height, country, region) VALUES
 ('Annapurna I', 8091, 'Nepal', 'Himalaya'),
 ('K5', 8080, 'Pakistan', 'Karakoram');
 
-INSERT INTO ascent (ascent_start, ascent_end, mountain_id, group_id) VALUES
-('1953-03-05', '1953-06-01', 1, 1),
-('2021-01-4', '2021-01-16', 2, 2),
-('1950-05-13', '1950-06-17', 5, 3),
-('1996-03-30', '1996-05-12', 1, 4),
-('1996-03-30', '1996-05-12', 1, 5),
-('1989-04-02', '1989-05-20', 3, 6);
-
 INSERT INTO `group` (name) VALUES
 ('Everest Expedition'),
 ('K2 Winter Ascent'),
@@ -21,6 +13,14 @@ INSERT INTO `group` (name) VALUES
 ('Mountain Madness'),
 ('Adventure Consultants'),
 ('Kangchenjunga Expedition');
+
+INSERT INTO ascent (ascent_start, ascent_end, mountain_id, group_id) VALUES
+('1953-03-05', '1953-06-01', 1, 1),
+('2021-01-4', '2021-01-16', 2, 2),
+('1950-05-13', '1950-06-17', 5, 3),
+('1996-03-30', '1996-05-12', 1, 4),
+('1996-03-30', '1996-05-12', 1, 5),
+('1989-04-02', '1989-05-20', 3, 6);
 
 INSERT INTO climber (name, address) VALUES
 ('Edmund Hillary', 'Auckland, New Zealand'),
@@ -38,8 +38,8 @@ INSERT INTO climber (name, address) VALUES
 ('Andy Harris', 'Salisbury, Wiltshire, England');
 
 INSERT INTO role (name) VALUES
-('Guide');
+('Lead'), ('Guide'), ('Sherpa'), ('Client'), ('Mountaineer');
 
 INSERT INTO climber_group VALUES
-(1, 1), (2, 1), (3, 2), (4, 2), (5, 2), (6, 2), (7, 2),
-(8, 3), (9, 3), (10, 4), (11, 4), (12, 5), (13, 5), (10, 6), (10, 1);
+(1, 1, 5), (2, 1, 3), (3, 2, 3), (4, 2, 3), (5, 2, 3), (6, 2, 3), (7, 2, 3),
+(8, 3, 5), (9, 3, 5), (10, 4, 2), (11, 4, 1), (12, 5, 2), (13, 5, 2), (10, 6, 5), (10, 1, 5);
